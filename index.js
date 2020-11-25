235d4ed8e245c4603eac3121b8f6da37ff84f79d
theboolean <cesana.lorenzo@gmail.com>
2020-11-25 15:00:43 +0100

    New: Rework logic to make workflow continue if not triggered by comment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { context, GitHub } = require('@actions/github')
 async function run() {
   const trigger = core.getInput('trigger', { required: true })
 
+  let triggered = false
   const reaction = core.getInput('reaction')
   const { GITHUB_TOKEN } = process.env
   if (reaction && !GITHUB_TOKEN) {
@@ -13,45 +14,37 @@ async function run() {
     return
   }
 
-  const body =
-    context.eventName === 'issue_comment'
-      ? context.payload.comment.body
-      : context.payload.pull_request.body
-  core.setOutput('comment_body', body)
+  if (context.eventName !== 'issue_comment') {
+    // If this is not a comment, workflow was triggered by some other hook.
+    // Make the workflow continue without any other check.
+    triggered = true
+  } else if (context.payload.issue.pull_request) {
+    // A comment in a PR triggered this workflow
+    const { body } = context.payload.comment
 
-  if (context.eventName === 'issue_comment' && !context.payload.issue.pull_request) {
-    // not a pull-request comment, aborting
-    core.setOutput('triggered', 'false')
+    // Check that comment contains the `trigger` word
+    const prefixOnly = core.getInput('prefix_only') === 'true'
+    if ((prefixOnly && body.startsWith(trigger)) || body.includes(trigger)) {
+      triggered = true
+    }
+  }
+
+  core.setOutput('triggered', triggered)
+
+  if (!triggered || !reaction) {
     return
   }
 
-  const { owner, repo } = context.repo
-
-  const prefixOnly = core.getInput('prefix_only') === 'true'
-  if ((prefixOnly && !body.startsWith(trigger)) || !body.includes(trigger)) {
-    core.setOutput('triggered', 'false')
-    return
-  }
-
-  core.setOutput('triggered', 'true')
-
-  if (!reaction) {
-    return
-  }
-
-  const client = new GitHub(GITHUB_TOKEN)
   if (context.eventName === 'issue_comment') {
+    // Add reaction to comment that triggered this workflow
+
+    const { owner, repo } = context.repo
+
+    const client = new GitHub(GITHUB_TOKEN)
     await client.reactions.createForIssueComment({
       owner,
       repo,
       comment_id: context.payload.comment.id,
-      content: reaction
-    })
-  } else {
-    await client.reactions.createForIssue({
-      owner,
-      repo,
-      issue_number: context.payload.pull_request.number,
       content: reaction
     })
   }
